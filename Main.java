4910949f643e2e31f37ac8fc15a81d53de2f934a
MauriH10 <mauridhernandez@gmail.com>
2025-11-22 13:51:51 -0300

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-import control.Controller;
-import javax.swing.UIManager;
-
-void main() throws Exception {
-    new Controller();
-    String lookAndFeelClassName = UIManager.getSystemLookAndFeelClassName();
-    UIManager.setLookAndFeel(lookAndFeelClassName);
-}
